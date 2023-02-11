9541482a88ee9efc7eba1be0413b001ab04f6d62
ghazih-z <ghaziharara@gmail.com>
2023-02-10 21:00:48 -0800

    try number 4

---
+++ ghazi.php
@@ -0,0 +1,11 @@
+<h1>HELLO PLANET EARTH</h1>
+<p>My name is Ghazi Harara</p>
+<img src=IDCard.jpg width=200>
+
+<p>
+<?php
+date_default_timezone_set('Asia/Kolkata');
+$date = date("d-m-y h:i:s");
+echo $date;
+?>
+</p>
